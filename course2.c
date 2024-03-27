500e785363b20aeadde8360a329839f1038331a9
Shreyansh-G <159716085+Shreyansh-G@users.noreply.github.com>
2024-03-27 13:22:42 +0530

    Add files via upload

---
+++ course2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+/*29-10-2023
+Program to get input between 0 and 1 (non inclusive) */
+int main(void)
+{
+  double radians;
+  double Result= 0;
+  printf("Input a number betwen 0 and 1:");
+  scanf("%lf",&radians);
+  Result=sin(radians);
+   if (0.00<radians && radians<1.00)
+    {
+     printf("\a The sine of %lf is %lf\n", radians, Result);
+    }
+   else
+    {
+      printf("You have written wrong information");
+    }
+ return 0; 
+}
